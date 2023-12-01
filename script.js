71e4cdb4e70cb64d9aa7cf0bc2f356a31d58852d
Benjamin Wallberg <me@bwallberg.com>
2023-12-01 22:07:13 +0100

    feat: parse years, months, weeks, days, hours seconds

---
+++ script.js
@@ -0,0 +1,39 @@
+const years = document.querySelector('span.years');
+const months = document.querySelector('span.months');
+const weeks = document.querySelector('span.weeks');
+const days = document.querySelector('span.days');
+const hours = document.querySelector('span.hours');
+const seconds = document.querySelector('span.seconds');
+
+function setDuration(iso8601) {
+	try {
+		const duration = Temporal.Duration.from(iso8601);
+
+	years.innerText = duration.years;
+	months.innerText = duration.months;
+	weeks.innerText = duration.weeks;
+	days.innerText = duration.days;
+	hours.innerText = duration.hours;
+	seconds.innerText = duration.seconds;
+	} catch(e) {
+		years.innerText = '-';
+		months.innerText = '-';
+		weeks.innerText = '-';
+		days.innerText = '-';
+		hours.innerText = '-';
+		seconds.innerText = '-';
+	}
+
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+	const input = document.querySelector('input');
+	input.focus();
+
+	input.addEventListener('input', ({ target: { value } }) => {
+		setDuration(value);
+	});
+	setDuration(input.value)
+});
+
